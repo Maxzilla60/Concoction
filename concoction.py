4b6d0e6f08ce2f45380b91a07987f3b69a7d24e0
Maxime Orione <maxpower98@hotmail.com>
2017-07-06 21:02:46 +0200

    Initial Commit

---
+++ concoction.py
@@ -0,0 +1,47 @@
+import sys, shutil, random
+# import dictionary
+from ingredients import ingredients_dictionary
+
+# Get input (arguments):
+if len(sys.argv) < 2:
+	print("Please provide a string as argument.")
+	exit()
+uhoh = len(sys.argv) > 2
+input = sys.argv[1]
+
+# Create file:
+print("Creating file concoction.chef...")
+recipe = open("concoction.chef", "w+")
+recipe.write("Strange Concoction.\n\n")
+
+# Ingredients:
+print("Parsing input...")
+recipe.write("Ingredients.\n")
+ingredients = {} # list of used ingredients
+# Parse input and make a list of used ingredients:
+for c in input:
+	ingredients[c] = ingredients_dictionary[c]
+
+print("Writing ingredients...")
+measures = ["ml", "l", "dashes", "cups", "teaspoons", "tablespoons"] # different measures to randomize
+items = list(ingredients.items()) # convert ingredients to list
+random.shuffle(items) # shuffle this list
+# Go through the ingredients and print 'em to the file:
+for key, value in items:
+	# "[ASCII value] [random measure] [ingredient name]"
+	recipe.write("" + str(ord(key)) + " " + random.choice(measures) + " " + value + "\n")
+recipe.write("\n")
+
+# Method:
+print("Writing method...")
+recipe.write("Method.\n")
+input = input[::-1] # reverse input
+for c in input:
+	recipe.write("Put " + ingredients[c] + " into mixing bowl. ")
+#recipe.write("\nPour contents of the mixing bowl into the baking dish. Refrigerate for 1 hours.")
+recipe.write("\nPour contents of the mixing bowl into the baking dish.\n\nServes 1.")
+
+recipe.close()
+print("Done!")
+if uhoh:
+	print("NOTICE: You've provided more than one argument, maybe you forgot to add around your given string?")
---
+++ ingredients.py
@@ -0,0 +1,97 @@
+ingredients_dictionary = {
+	#'\n' : 'new lines', #
+	' ' : 'air', #
+	'!' : 'gunpowder', #
+	'"' : 'quotes',
+	'#' : 'hashtags',
+	'$' : 'gold', #
+	'%' : 'percents',
+	'&' : 'glue', #
+	"'" : 'apostrophes',
+	'(' : 'salt', #
+	')' : 'pepper', #
+	'*' : 'sprinkles', #
+	'+' : 'plusses',
+	',' : 'food coloring', #
+	'-' : 'hyphens',
+	'.' : 'bread crumbs', #
+	'/' : 'slashes',
+	'0' : 'zeroes',
+	'1' : 'ones',
+	'2' : 'twos',
+	'3' : 'threes',
+	'4' : 'fours',
+	'5' : 'fives',
+	'6' : 'sixes',
+	'7' : '7Up', #
+	'8' : 'eights',
+	'9' : 'nines',
+	':' : 'hemorrhoids creme', #
+	';' : 'toothpaste', #
+	'<' : 'less-thans',
+	'=' : 'equals',
+	'>' : 'more-thans',
+	'?' : 'stuff', #
+	'@' : 'Ats',
+	'A' : 'sparkling water', #
+	'B' : 'rum', #
+	'C' : 'coffee', #
+	'D' : 'D',
+	'E' : 'egg whites', #
+	'F' : 'baking powder',
+	'G' : 'G',
+	'H' : 'H',
+	'I' : 'paint', #
+	'J' : 'fruit juice', #
+	'K' : 'K',
+	'L' : 'lemon juice', #
+	'M' : 'soy milk', #
+	'N' : 'N',
+	'O' : 'olive oil', #
+	'P' : 'Coca-Cola', #
+	'Q' : 'Q',
+	'R' : 'R',
+	'S' : 'soap', #
+	'T' : 'tomato juice', #
+	'U' : 'U',
+	'V' : 'V',
+	'W' : 'red wine', #
+	'X' : 'X',
+	'Y' : 'cream cheese', #
+	'Z' : 'Z',
+	'[' : 'open brackets',
+	'\\' : 'backslashes',
+	']' : 'closed brackets',
+	'^' : 'circumflexes',
+	'_' : 'underscores',
+	'`' : 'grave accents',
+	'a' : 'water', #
+	'b' : 'beer', #
+	'c' : 'chocolate', #
+	'd' : 'd',
+	'e' : 'egg yolk', #
+	'f' : 'flour',
+	'g' : 'g',
+	'h' : 'honey', #
+	'i' : 'ink', #
+	'j' : 'orange juice', #
+	'k' : 'k',
+	'l' : 'lemonade', #
+	'm' : 'milk', #
+	'n' : 'n',
+	'o' : 'oil', #
+	'p' : 'Pepsi', #
+	'q' : 'q',
+	'r' : 'r',
+	's' : 'sugar', #
+	't' : 'tabasco', #
+	'u' : 'u',
+	'v' : 'vinegar', #
+	'w' : 'white wine', #
+	'x' : 'x',
+	'y' : 'yoghurt', #
+	'z' : 'z',
+	'{' : 'ketchup', #
+	'|' : 'pipes', #
+	'}' : 'mayonnaise' #
+}
